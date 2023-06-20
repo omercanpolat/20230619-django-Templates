c7b266c7d8f74c7e6eb465fd0becdd0975de34f5
omer canpolat <114859630+omercanpolat@users.noreply.github.com>
2023-06-20 23:46:39 +0300

    last updates

--- home/views.py
+++ home/views.py
@@ -22,3 +22,66 @@ def student_detail(request):
         'students': students
     }
     return render(request, 'home/student_detail.html', context)
+
+
+
+
+from .forms import StudentForm
+from django.shortcuts import redirect
+
+def student_add(request):
+    form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    context = {
+        'form':form
+    }
+    return render(request, 'home/student_add.html', context)
+
+
+from django.views.generic import (
+    CreateView, 
+    ListView,
+    DetailView,
+    UpdateView,
+    DeleteView
+)
+from django.urls import reverse_lazy
+
+class StudentAddView(CreateView):
+    model = Student
+    form_class = StudentForm
+    success_url = reverse_lazy('list')
+    # defaults to 'home/student_form.html'
+    template_name = 'home/student_add.html'
+
+
+class StudentListView(ListView):
+    model = Student
+    # defaults to 'home/student_form.html'
+    template_name = 'home/student_list.html'
+    # paginate_by = 5
+
+
+class StudentDetailView(DetailView):
+    model = Student
+    # pk_url_kwarg = 'id'
+    template_name = 'home/student_detail_2.html'
+    context_object_name = 'student'
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    # pk_url_kwarg = 'id'
+    context_object_name = 'student'
+    success_url = reverse_lazy('list')
+
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    # pk_url_kwarg = 'id'
+    success_url = reverse_lazy('list')
--- main/settings.py
+++ main/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-4gk-g=h#laax$#@zopt8(@*erenl7%$j#0_za$!#=y7=58-%6('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    #Apps
     'home',
 ]
 
@@ -120,6 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
